372083565b4237eeb7f101d9a9d53b54a6f13fcc
Miro Mannino <miro.mannino@gmail.com>
2024-09-29 10:32:40 +0400

    test collapse changes

--- tests/test_collapse_changes.py
+++ tests/test_collapse_changes.py
@@ -1,42 +1,58 @@
-import git
-from src import *
-import shutil
-from tests.tests_commons import REPOS_PATHS, MOCK_REPO_PATH
-import pytest
 import os
+import shutil
+import git
+import pytest
+from src.ImporterFromRepository import ImporterFromRepository
+from tests.tests_commons import REPOS_PATHS, MOCK_REPO_PATH
 
 
-def test_collapse_changes_smaller_filesizes():
+@pytest.fixture
+def setup_mock_repos():
+  # Cleanup existing mock repos
+  shutil.rmtree(MOCK_REPO_PATH, ignore_errors=True)
+  shutil.rmtree(MOCK_REPO_PATH + "_c", ignore_errors=True)
+  yield
+  # Cleanup after the test
+  shutil.rmtree(MOCK_REPO_PATH, ignore_errors=True)
+  shutil.rmtree(MOCK_REPO_PATH + "_c", ignore_errors=True)
+
+
+def test_collapse_changes_smaller_filesizes(setup_mock_repos):
+  # Initialize repositories
   repos = [git.Repo(repo_path) for repo_path in REPOS_PATHS]
 
-  shutil.rmtree(MOCK_REPO_PATH + '_c', ignore_errors=True)
-  mock_repo_collapsed = git.Repo.init(MOCK_REPO_PATH + '_c')
-  importer = ImporterFromRepository(repos, mock_repo_collapsed)
-  importer.set_collapse_multiple_changes_to_one(True)
-  importer.set_keep_commit_messages(True)
-  importer.import_repository()
+  # Test with collapse enabled
+  mock_repo_collapsed = git.Repo.init(MOCK_REPO_PATH + "_c")
+  importer_collapsed = ImporterFromRepository(repos, mock_repo_collapsed)
+  importer_collapsed.set_collapse_multiple_changes_to_one(True)
+  importer_collapsed.set_keep_commit_messages(True)
+  importer_collapsed.import_repository()
 
-  shutil.rmtree(MOCK_REPO_PATH, ignore_errors=True)
+  # Test with collapse disabled
   mock_repo = git.Repo.init(MOCK_REPO_PATH)
-  importer = ImporterFromRepository(repos, mock_repo)
-  importer.set_collapse_multiple_changes_to_one(False)
-  importer.set_keep_commit_messages(True)
-  importer.import_repository()
+  importer_non_collapsed = ImporterFromRepository(repos, mock_repo)
+  importer_non_collapsed.set_collapse_multiple_changes_to_one(False)
+  importer_non_collapsed.set_keep_commit_messages(True)
+  importer_non_collapsed.import_repository()
 
-  files_collapsed = os.listdir(MOCK_REPO_PATH + '_c')
-  files_collapsed = list(filter(lambda f: os.path.isfile(f), files_collapsed))
-  files_collapsed.remove("README.md")
-  files_non_collapsed = os.listdir(MOCK_REPO_PATH)
-  files_non_collapsed = list(filter(lambda f: os.path.isfile(f), files_non_collapsed))
-  files_non_collapsed.remove("README.md")
+  # Get lists of files in each mock repo, excluding README.md
+  files_collapsed = [
+      f for f in os.listdir(MOCK_REPO_PATH + "_c")
+      if os.path.isfile(os.path.join(MOCK_REPO_PATH + "_c", f)) and not f.endswith(".md")
+  ]
+  files_non_collapsed = [
+      f for f in os.listdir(MOCK_REPO_PATH)
+      if os.path.isfile(os.path.join(MOCK_REPO_PATH, f)) and not f.endswith(".md")
+  ]
+
+  # Assert same number of files
   assert len(files_collapsed) == len(files_non_collapsed)
 
+  # Compare line counts for each file
   for file in files_collapsed:
     assert file in files_non_collapsed
-    lines_of_code_collapsed = 0
-    lines_of_code_non_collapsed = 0
-    with open(os.path.join(MOCK_REPO_PATH + '_c', file), 'r') as f:
-      lines_of_code_collapsed = len(f.readlines())
-    with open(os.path.join(MOCK_REPO_PATH, file), 'r') as f:
-      lines_of_code_non_collapsed = len(f.readlines())
-    assert lines_of_code_collapsed < lines_of_code_non_collapsed
+    with open(os.path.join(MOCK_REPO_PATH + "_c", file), "r") as collapsed_file:
+      lines_collapsed = len(collapsed_file.readlines())
+    with open(os.path.join(MOCK_REPO_PATH, file), "r") as non_collapsed_file:
+      lines_non_collapsed = len(non_collapsed_file.readlines())
+    assert lines_collapsed < lines_non_collapsed
